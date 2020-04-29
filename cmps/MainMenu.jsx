30ccc218919262d9a17b420cdb2e4b4ca4de2838
reemnx <37637982+reemnx@users.noreply.github.com>
2020-04-29 19:30:42 +0300

    after main meny design

--- cmps/MainMenu.jsx
+++ cmps/MainMenu.jsx
@@ -16,12 +16,12 @@ export default class MainMenu extends React.Component {
         const { isMenuShown } = this.state
 
         return (
-            <div className="flex row-reverse">
-                <button onClick={this.onMenuToggle}>Icon</button>
-                {isMenuShown && <div className="apps-container">
-                    <NavLink exact to="/misskeep">K</NavLink>
-                    <NavLink exact to="/email">E</NavLink>
-                    <NavLink exact to="/missbooks/welcome">B</NavLink>
+            <div className="flex relative row-reverse">
+                <img className="MK-menu" onClick={this.onMenuToggle} />
+                {isMenuShown && <div className="apps-container flex space-between align-center">
+                    <NavLink exact to="/misskeep"><img className="MK-logo" /></NavLink>
+                    <NavLink exact to="/email"><img className="MK-Gmail" /></NavLink>
+                    <NavLink exact to="/missbooks/welcome"><img className="MK-books" /></NavLink>
                 </div>}
             </div>
         )
